fe207f76ecb3c6dba0cb3588bad8f52a175ef53b
KiraElric <lissette.urso@gmail.com>
2021-07-07 00:41:09 -0400

    Creación y ejecucion del seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Type.destroy_all
+Category.destroy_all
+Bookmark.destroy_all
+
+type = ['Documento', 'Video', 'Infografia', 'Diagrama', 'Entrevista', 'Reporte']
+
+category = ['Ciencia', 'Literatura', 'Hobbies']
+subcategories = [['Fisica', 'Quimica', 'Biologia'], ['Ciencia Ficción', 'Fantasia', 'Novela'], 
+                  ['Pintura', 'Tejidos', 'Maquetas']]
+
+type.each do |t|
+  ty = Type.create(name: t)
+  puts "Se ha creado el tipo: #{ty.name}"
+end
+
+category.each do |c|
+  ca = Category.create(name: c)
+  puts "Se ha creado la categoria: #{ca.name}"
+end
+
+3.times do |i|
+  subcategories[i].each do |s|
+    su = Category.create(name: s, parent_category: i+1)
+    puts "Se ha creado la subcategoria: #{su.name}"
+  end
+end
+
+20.times do |i|
+  bookmark = Bookmark.create(name: Faker::Superhero.name ,
+                        url: Faker::Internet.url,
+                        category_id: rand(1..12),
+                        type_id: rand(1..6))
+  puts "Se ha creado el marcador: #{bookmark.name}"
+end
