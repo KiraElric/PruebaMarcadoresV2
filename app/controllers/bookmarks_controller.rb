82e122fd073399934c586dd762ef1f1f814be003
KiraElric <lissette.urso@gmail.com>
2021-07-07 12:57:27 -0400

    CRUD Bookmark con Ujs

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,19 @@
 class BookmarksController < ApplicationController
   before_action :set_bookmark, only: %i[ show edit update destroy ]
-  before_action :set_categories, :set_types, only: %i[ new edit ]
+  before_action :set_categories, :set_types, only: %i[ new edit index ]
 
   # GET /bookmarks or /bookmarks.json
   def index
     @bookmarks = Bookmark.all
+    @bookmark = Bookmark.new
     @bookmark_group = Bookmark.group(:type_id).count.transform_keys {|key| Type.find(key).name}
-    
   end
 
   # GET /bookmarks/1 or /bookmarks/1.json
   def show
+    respond_to do |format|
+      format.js
+    end
   end
 
   # GET /bookmarks/new
@@ -20,16 +23,22 @@ class BookmarksController < ApplicationController
 
   # GET /bookmarks/1/edit
   def edit
+    respond_to do |format|
+      format.js
+    end
   end
 
   # POST /bookmarks or /bookmarks.json
   def create
     @bookmark = Bookmark.new(bookmark_params)
 
+    sleep 1
+    
     respond_to do |format|
       if @bookmark.save
         format.html { redirect_to @bookmark, notice: "Bookmark was successfully created." }
         format.json { render :show, status: :created, location: @bookmark }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @bookmark.errors, status: :unprocessable_entity }
@@ -43,6 +52,7 @@ class BookmarksController < ApplicationController
       if @bookmark.update(bookmark_params)
         format.html { redirect_to @bookmark, notice: "Bookmark was successfully updated." }
         format.json { render :show, status: :ok, location: @bookmark }
+        format.js
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @bookmark.errors, status: :unprocessable_entity }
@@ -56,6 +66,7 @@ class BookmarksController < ApplicationController
     respond_to do |format|
       format.html { redirect_to bookmarks_url, notice: "Bookmark was successfully destroyed." }
       format.json { head :no_content }
+      format.js
     end
   end
 
